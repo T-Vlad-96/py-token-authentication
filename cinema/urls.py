b8c00661e31a6cd0c24189e708048641c7ec8778
user <user@MacBook-Air-user.local>
2026-03-17 13:25:51 +0200

    feat: set only ('list', 'create') operations as available for CinemaHallViewSet

--- cinema/urls.py
+++ cinema/urls.py
@@ -12,14 +12,22 @@ from cinema.views import (
 
 router = routers.DefaultRouter()
 router.register("actors", ActorViewSet)
-router.register("cinema_halls", CinemaHallViewSet)
 router.register("movies", MovieViewSet)
 router.register("movie_sessions", MovieSessionViewSet)
 router.register("orders", OrderViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("genres/", GenreViewSet.as_view(), name="genre-list"),
+    path(
+        "genres/",
+        GenreViewSet.as_view(),
+        name="genre-list"
+    ),
+    path(
+        "cinema_halls/",
+        CinemaHallViewSet.as_view(),
+        name="cinemahall-list"
+    ),
 ]
 
 app_name = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -34,7 +34,7 @@ class ActorViewSet(viewsets.ModelViewSet):
     serializer_class = ActorSerializer
 
 
-class CinemaHallViewSet(viewsets.ModelViewSet):
+class CinemaHallViewSet(ListCreateAPIView):
     queryset = CinemaHall.objects.all()
     serializer_class = CinemaHallSerializer
 
@@ -85,8 +85,8 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         .select_related("movie", "cinema_hall")
         .annotate(
             tickets_available=F("cinema_hall__rows")
-            * F("cinema_hall__seats_in_row")
-            - Count("tickets")
+                              * F("cinema_hall__seats_in_row")
+                              - Count("tickets")
         )
     )
     serializer_class = MovieSessionSerializer
