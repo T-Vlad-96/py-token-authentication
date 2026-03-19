51267a8568a24f24b36b44e0fee99d27537fc508
user <user@MacBook-Air-user.local>
2026-03-19 13:49:22 +0200

    fix: remove bug in UserSerializer.update() method

--- user/serializers.py
+++ user/serializers.py
@@ -21,7 +21,7 @@ class UserSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
-        password = validated_data.pop("password", None)
+        password = validated_data.pop("password")
         user = get_user_model().objects.create_user(
             **validated_data
         )
@@ -30,7 +30,7 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
     def update(self, instance, validated_data):
-        password = validated_data.pop("password")
+        password = validated_data.pop("password", None)
         instance = super().update(
             instance=instance,
             validated_data=validated_data
